2f32c47316d1ce68b9df8185031f6a8785c424fe
Connor <ces156@pitt.edu>
2022-09-26 21:41:26 -0400

    Added Lab0

---
+++ Lab0/SortData.java
@@ -0,0 +1,46 @@
+package Lab0;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SortData {
+    public static void main(String[] args) {
+        int [] numbers = new int[100];
+
+        //TODO: Get Input from input.txt, store values in array
+        File inputFile = new File("input.txt");
+        if (inputFile.exists()) {
+            try {
+                Scanner fin = new Scanner(inputFile);
+
+                for(int i = 0; i < 100; i++) {
+                    numbers[i] = fin.nextInt();
+                }
+                
+                fin.close();
+            
+            } catch (FileNotFoundException ex) {
+                System.out.println("File Input Error");
+            }
+        } else {
+            System.out.println("File not found");
+        }
+
+        //TODO: Sort the array. HINT: Look at your imports.
+        Arrays.sort(numbers);
+
+        //TODO: Write output to output.txt
+        try {
+            File outputFile = new File("output.txt");
+            PrintWriter fout = new PrintWriter(outputFile);
+            for (int i = 0; i < 100; i++) {
+                fout.println(numbers[i]);
+            }
+        fout.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Output Error");
+        }
+    }
+}
